efc7603276955cbf6de02e4675cb56d05f39c1f8
Timur Celikel <tcelikel@gmail.com>
2015-08-08 16:50:02 +0000

    added sl4j dependency

---
+++ src/main/java/sandbox/SandboxRunner.java
@@ -0,0 +1,11 @@
+package sandbox;
+
+public class SandboxRunner {
+  
+  public static void main(String[] args) {
+    
+    // Static Class Initialization
+    StaticInitialization.main(args);
+   
+  }
+}
