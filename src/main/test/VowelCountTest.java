ff6618b5ee3975b298dbc94dde1ea3866be8af84
Timur Celikel <tcelikel@gmail.com>
2015-08-11 05:27:24 +0000

    added codewars package and vowelcount kata

---
+++ src/main/test/VowelCountTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import codewars.VowelCount;
+
+public class VowelCountTest {
+    @Test
+    public void testCase1() {
+      assertEquals("Nope!", 5, VowelCount.getCount("abracadabra"));
+    }
+    
+}
