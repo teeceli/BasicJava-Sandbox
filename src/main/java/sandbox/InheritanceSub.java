3d4d39078dbb82336b1a4816af81bf3639dd78e9
Timur Celikel <tcelikel@gmail.com>
2015-08-14 04:10:13 +0000

    added inheritance example and found error in Oracle: Java Complete Reference book

---
+++ src/main/java/sandbox/InheritanceSub.java
@@ -0,0 +1,9 @@
+package sandbox;
+
+public class InheritanceSub extends InheritanceSuper {
+  
+	int total;
+	void sum() {
+		total = i + j; 	// ERROR, j is not accessible here; i can be referenced directly without initialization
+	} 
+}
