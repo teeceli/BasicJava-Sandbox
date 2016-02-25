7c158b36634b45ae4b76e8242dba2bf069dd9612
teeceli <tcelikel@gmai.com>
2016-02-24 22:02:10 -0800

    fixed a bug with not calculating best profit

--- src/main/java/interviewcake/InterviewCake1.java
+++ src/main/java/interviewcake/InterviewCake1.java
@@ -13,7 +13,6 @@ import sandbox.StaticInitialization;
  * https://www.interviewcake.com/question/java/stock-price
  * Solution on web site is a little more elegant because of not needing to 
  * initialize bestProfit to -9999 but I think mine is easier to read. 
- * Still satisfies O(n) time complexity
  */
 
 public class InterviewCake1 {
@@ -22,7 +21,8 @@ public class InterviewCake1 {
 
 	public static void main(String[] args) {
 				
-		List<Integer> stockPricesYesterday = Arrays.asList(15, 7, 5, 4, 2, 0);
+		List<Integer> stockPricesYesterday = Arrays.asList(10, 7, 5, 8, 11, 9);			// Should be 6
+		//List<Integer> stockPricesYesterday = Arrays.asList(10, 7, 5, 4, 3, 2, 0);		// Should be -1
 		
 		Integer bestProfit = getMaxProfit(stockPricesYesterday);
 		
@@ -32,16 +32,23 @@ public class InterviewCake1 {
 
 	private static Integer getMaxProfit(List<Integer> stockPricesYesterday) {
 		
-		Integer profit = -9999;
+		Integer currentBestProfit = -9999;
+		Integer minPrice = stockPricesYesterday.get(0);
 		
 		for (int i = 0; i < stockPricesYesterday.size() - 1; i++) {
-			Integer tmpProfit = stockPricesYesterday.get(i+1) - stockPricesYesterday.get(i);
-			if (tmpProfit > profit) {
-				profit = tmpProfit;
+			
+			if (stockPricesYesterday.get(i) < minPrice) {
+				minPrice = stockPricesYesterday.get(i);
+			}
+
+			Integer tmpProfit = stockPricesYesterday.get(i+1) - minPrice;
+			
+			if (tmpProfit > currentBestProfit) {
+				currentBestProfit = tmpProfit;
 			}
 		}
 		
-		return profit;
+		return currentBestProfit;
 	}
 
 }
